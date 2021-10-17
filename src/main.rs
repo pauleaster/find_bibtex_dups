b2e8adc8dda6f0fccb59bb064080e51d82d571be
Paul Easter <paul.john.easter@gmail.com>
2021-10-17 21:03:36 +1100

    Worked out how to read the individual fields.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{fs, io::{self, Error}};
-use biblatex::Bibliography;
+use biblatex::{Bibliography, Chunk};
 
 fn read_bibliography_file(path: String) -> Result<Bibliography, Error>{
     let bibtex_file = fs::read_to_string(path)?;
@@ -14,6 +14,24 @@ fn read_bibliography_file(path: String) -> Result<Bibliography, Error>{
 fn main() -> Result<(),Error>{
     let bib = read_bibliography_file("FinalBib.bib".to_string())?;
     
-   println!("{:?}",bib);
-   Ok(())
+//    println!("{:?}",bib);
+    let mut i=0;
+
+    for  bib1 in bib {
+        let fields = bib1.fields;
+        if let Some(pages) = fields.get("pages") {
+            // println!("{:?}",&pages);
+            if let Chunk::Normal(s) = &pages[0] {
+                println!("{}",&s);
+            }
+
+            i += 1;
+
+        }
+    
+
+    }
+
+
+    Ok(())
 }
